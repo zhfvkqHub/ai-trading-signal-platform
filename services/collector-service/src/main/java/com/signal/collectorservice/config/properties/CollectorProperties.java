cad05a397a2073c8102f42e861a51c36e9996971
김다연 <hamoey@naver.com>
2026-03-26 08:59:37 +0900

    feat: 프리마켓 kis 수집 재거

--- services/collector-service/src/main/java/com/signal/collectorservice/config/properties/CollectorProperties.java
+++ services/collector-service/src/main/java/com/signal/collectorservice/config/properties/CollectorProperties.java
@@ -17,7 +17,6 @@ public class CollectorProperties {
     @Getter
     @Setter
     public static class Schedule {
-        private CronEntry preMarket = new CronEntry();
         private CronEntry market = new CronEntry();
         private CronEntry afterHours = new CronEntry();
         private CronEntry news = new CronEntry();
